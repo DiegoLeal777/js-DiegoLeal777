eda922d372b938088958f6bdf5c3ecb259355435
DiegoLeal777 <diegophh7@gmail.com>
2024-08-28 15:45:44 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,47 @@
+function setup() {
+    createCanvas(400, 330);
+  }
+  
+  
+  function draw() {
+    background(220);     
+  }
+  function draw() {
+      background("blue");
+    
+  
+    
+    fill("red");
+       rect(125, -2, 70, 400), strokeWeight(0);
+    
+    fill("red");
+       rect(0, 125, 400, 100);
+  
+  
+     fill("white");
+       rect(185, 185, 400, 40);
+    
+    fill("white");
+       rect(185, 200, 40, 400);
+    
+     fill("white");
+       rect(85, 200, 40, 400);
+    
+     fill("white");
+       rect(0, 185, 125, 50);
+    
+    
+     fill("white");
+       rect(85, 0, 40, 125);
+    
+    fill("white");
+       rect(0, 90, 125, 40);
+     
+    fill("white");
+       rect(185, 80, 390, 45);
+    
+    
+     fill("white");
+       rect(185, 0, 40, 110);
+    
+  }
